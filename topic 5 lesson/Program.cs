9d0e029e89c3cbd1d14d903aa16961689d7b7afd
Jonathan Gaboury <362568651@GoTVDSB.ca>
2024-09-20 09:27:41 -0400

    Add project files.

---
+++ topic 5 lesson/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace topic_5_lesson
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // jonathan
+            int people = 20;
+            int cats = 30;
+            int dog = 15;
+            Console.WriteLine("people: " + people + " dogs:" + dog + " cats: " + cats);
+            if (people < cats)
+            {
+                Console.WriteLine("too many cats!!!");
+            }
+            if (people > cats)
+            {
+                Console.WriteLine("not many cats");
+            }
+            if (people < dog)
+            {
+                Console.WriteLine("too many dogs");
+            }
+            if (people > dog)
+            {
+                Console.WriteLine("not many dogs");
+            }
+            Console.WriteLine("press Enter to continue");
+            Console.ReadLine();
+            Console.Clear();
+
+        }
+    }
+}
